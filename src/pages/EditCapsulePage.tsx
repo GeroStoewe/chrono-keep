2a04829436a37aefcde7ee6f6e262d98c9e66d10
Aylin Oymak <aylin.oymak@safetyio.com>
2025-03-13 23:27:19 +0100

    add handle status change to set the release date automatically today, if the capsule is manually unlocked

--- src/pages/EditCapsulePage.tsx
+++ src/pages/EditCapsulePage.tsx
@@ -125,6 +125,18 @@ function EditCapsulePage() {
     setSnackbar({ ...snackbar, open: false });
   };
 
+  // Handle status change
+  const handleStatusChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newStatus = e.target.value;
+    setStatus(newStatus);
+
+    // Automatically set release date to today if status is "unlocked"
+    if (newStatus === "unlocked") {
+      const today = new Date().toISOString().split("T")[0]; // Get today's date in YYYY-MM-DD format
+      setReleaseDate(today);
+    }
+  };
+
   return (
     <div className="min-h-screen bg-gradient-to-tl from-blue-400 to-purple-700">
       <NavigationBar />
@@ -174,7 +186,7 @@ function EditCapsulePage() {
               <div className="relative">
                 <select
                   value={status}
-                  onChange={(e) => setStatus(e.target.value)}
+                  onChange={handleStatusChange}
                   className="w-full px-4 py-3 pr-100 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-transparent transition duration-300 transform hover:scale-105 focus:gradient-border appearance-none"
                   required
                 >
